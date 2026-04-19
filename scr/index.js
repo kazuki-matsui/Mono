b0df89fed35358f6573483f74332fe56156b0fa5
kazuki-matsui <thecatyko@gmail.com>
2026-04-19 14:25:45 +0200

    Pushing stuff Updated variables.json

--- scr/index.js
+++
@@ -1,8 +0,0 @@
-const { load } = require("./loader");
-const AdmZip = require("adm-zip");
-
-const data = (new AdmZip("World.mono"));
-const apple = JSON.parse(data.getEntry("citrus/item/apple.json").getData());
-
-const item = require('./World/citrus/item/apple.json');
-console.log(apple);
---
+++ src/index.js
@@ -0,0 +1,2 @@
+const apple = require('./World/citrus/item/apple.json');
+console.log(apple);
