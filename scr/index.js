960c701aa039d9f1cc16f1b0f1f49716476b180a
kazuki-matsui <thecatyko@gmail.com>
2026-04-19 11:23:56 +0200

    w

--- scr/index.js
+++ scr/index.js
@@ -4,4 +4,4 @@ const data = new AdmZip("World.mono");
 const apple = JSON.parse(data.getEntry("citrus/item/apple.json").getData());
 
 const item = require('./World/citrus/item/apple.json');
-console.log(apple.rarity);
+console.log(apple);
