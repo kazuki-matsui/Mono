32b59ff20afe37d8bef7ff714b72948054655efa
kazuki-matsui <thecatyko@gmail.com>
2026-04-20 18:40:18 +0200

    changed gitignore

--- src/register-commands.js
+++ src/register-commands.js
@@ -44,6 +44,10 @@ const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
         console.log('Registering slash commands...');
         await rest.put(
             Routes.applicationCommands(process.env.CLIENT_ID),
+            {body: null}
+        );
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
             {body: commands}
         );
         console.log('Slash commands were registered successfully!');
