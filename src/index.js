5cbfc11c9f1d3279c70c8ab6a145b582d762e599
kazuki-matsui <thecatyko@gmail.com>
2026-04-25 12:50:28 +0200

    Made changes to commands. Tried testing link buttons and button collectors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const {
     Client,
-    IntentsBitField
+    IntentsBitField,
+    ButtonBuilder,
+    ButtonStyle,
+    ActionRowBuilder,
+    ComponentType,
 } = require('discord.js');
 
 const client = new Client({
@@ -16,9 +20,45 @@ const client = new Client({
 client.on('ready', (client) => {
     console.log(`${client.user.username}`);
 })
-client.on('interactionCreate', (interaction) => {
+let buttonValue = new Map();
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
     if (interaction.commandName === 'home') {
-        interaction.reply('hello');
+        const redButton = new ButtonBuilder()
+            .setLabel('Red')
+            .setEmoji('🍎')
+            .setStyle(ButtonStyle.Danger)
+            .setCustomId('red');
+        const greenButton = new ButtonBuilder()
+            .setLabel('Green')
+            .setEmoji('🍏')
+            .setStyle(ButtonStyle.Success)
+            .setCustomId('green');
+        const linkButton = new ButtonBuilder()
+            .setLabel('Link')
+            .setEmoji('🗞️')
+            .setStyle(ButtonStyle.Link)
+            .setURL('https://discord.com/channels/1228073258948231291/1496886786796949657');
+        const actionRow = new ActionRowBuilder().addComponents(redButton, greenButton, linkButton);
+        const reply = await interaction.reply({content: '# Home 🏡', components: [actionRow]});
+
+        const filter = (interaction) => interaction.user.id === interaction.author.id;
+        const collector = reply.createMessageComponentCollector({
+            componentType: ComponentType.Button,
+            filter,
+        });
+        collector.on('collect', (i) => {
+            if(i.customId === 'red') {
+                buttonValue.set(i.user.id, 'Red');
+                i.reply(buttonValue.get(i.user.id));
+            } else if (i.customId === 'green') {
+                buttonValue.set(i.user.id, 'Green');
+                i.reply(buttonValue.get(i.user.id));
+            }
+        });
+    }
+    if (interaction.commandName === 'cheats') {
+        interaction.reply(buttonValue.get(interaction.user.id) ?? 'No selection yet');
     }
 });
 
--- src/register-commands.js
+++ src/register-commands.js
@@ -44,10 +44,6 @@ const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
         console.log('Registering slash commands...');
         await rest.put(
             Routes.applicationCommands(process.env.CLIENT_ID),
-            {body: null}
-        );
-        await rest.put(
-            Routes.applicationCommands(process.env.CLIENT_ID),
             {body: commands}
         );
         console.log('Slash commands were registered successfully!');
