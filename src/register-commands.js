676c3149447252e959d6c3f3c7cf6e64ff978857
kazuki-matsui <thecatyko@gmail.com>
2026-05-02 15:22:54 +0200

    Made /cheats command create Audio channel and then loop music inside it. Added music and sound folder to World (data) so the program has a place where to grab music and sounds

--- src/register-commands.js
+++ src/register-commands.js
@@ -10,19 +10,19 @@ const commands = [
         name: 'cheats',
         description: 'Opens the cheat menu',
     }
-];
+]
 
-const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
+const rest = new REST({version: '10'}).setToken(process.env.TOKEN)
 
 (async () => {
     try {
-        console.log('Registering slash commands...');
+        console.log('Registering slash commands...')
         await rest.put(
             Routes.applicationCommands(process.env.CLIENT_ID),
             {body: commands}
         );
-        console.log('Slash commands were registered successfully!');
+        console.log('Slash commands were registered successfully!')
     } catch (error) {
-        console.log(`${error}`);
+        console.log(`${error}`)
     }
-})();
+})()
