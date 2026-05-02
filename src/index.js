5b02b5a9fc6ce769d3eaa5dfd68ec2996a24e48f
kazuki-matsui <thecatyko@gmail.com>
2026-05-02 15:15:58 +0200

    Made /cheats command create Audio channel and then loop music inside it. Added music and sound folder to World (data) so the program has a place where to grab music and sounds

--- src/index.js
+++ src/index.js
@@ -2,27 +2,38 @@ require('dotenv').config();
 let variables = require('./World/citrus/variables.json');
 const {
     Client,
-    IntentsBitField,
+    GatewayIntentBits,
     StringSelectMenuBuilder,
     StringSelectMenuOptionBuilder,
     ActionRowBuilder,
     ComponentType,
+    ButtonBuilder,
+    ButtonStyle,
+    ChannelType
 } = require('discord.js');
-
+const {
+    joinVoiceChannel,
+    createAudioPlayer,
+    createAudioResource,
+    StreamType,
+    AudioPlayerStatus
+} = require('@discordjs/voice');
+const { createReadStream } = require('fs');
 const client = new Client({
     intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates
     ]
 });
 
 client.on('clientReady', (client) => {
-    console.log(`${client.user.username}`);
+    console.log(`${client.user.username}`)
 })
 client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return
     if (interaction.commandName === 'home') {
         const selectMenu = new StringSelectMenuBuilder()
             .setCustomId('colors')
@@ -33,34 +44,80 @@ client.on('interactionCreate', async (interaction) => {
                 new StringSelectMenuOptionBuilder().setEmoji('💙').setValue('blue').setLabel('Blue'),
                 new StringSelectMenuOptionBuilder().setEmoji('💚').setValue('green').setLabel('Green'),
                 new StringSelectMenuOptionBuilder().setEmoji('💛').setValue('yellow').setLabel('Yellow'),
-                new StringSelectMenuOptionBuilder().setEmoji('🧡').setValue('orange').setLabel('Orange'));
-        const actionRow = new ActionRowBuilder().addComponents(selectMenu);
-        const reply = await interaction.reply({content: "# Choose", components: [actionRow]});
+                new StringSelectMenuOptionBuilder().setEmoji('🧡').setValue('orange').setLabel('Orange'))
+        const actionRow = new ActionRowBuilder().addComponents(selectMenu)
+        const reply = await interaction.reply({content: "# Choose", components: [actionRow]})
 
         const collector = reply.createMessageComponentCollector({
             componentType: ComponentType.StringSelect,
 
-        });
+        })
         collector.on('collect', (interactionSelectMenu) => {
-            variables.playerInventory.base[interactionSelectMenu.user.id] = interactionSelectMenu.values;
-            console.log(variables);
-            interactionSelectMenu.reply('## Colors Chosen: ' + interactionSelectMenu.values)
+            variables.playerInventory.base[interactionSelectMenu.user.id] = interactionSelectMenu.values
+            interactionSelectMenu.reply('## Colors Chosen: ' + variables.playerInventory.base[interactionSelectMenu.user.id].toString())
         })
     }
     if (interaction.commandName === 'cheats') {
-        try{
-            const variables = variables.playerInventory.base[interaction.user.id];
-            if (!variables) {
-                await interaction.reply('No selection yet');
-                return;
-            }
-            await interaction.reply(`Current selection: ${variables.selected}`);
-        }
-        catch (error) {
-            console.error('Error handling /cheats command:', error);
-            await interaction.reply('An error occurred while processing your command.');
-        }
-    }
-});
+        const voiceChannel = await interaction.guild.channels.create({
+            name: '𝖠udio',
+            type: ChannelType.GuildVoice,
+            parent: interaction.channel.parent.id,
+            permissionOverwrites: [
+                {
+                    id: interaction.guild.roles.everyone.id,
+                    deny: ['ViewChannel', 'Connect']
+                },
+                {
+                    id: interaction.user.id,
+                    allow: ['ViewChannel', 'Connect', 'Speak'],
+                    deny: ['UseEmbeddedActivities']
+                }
+            ]
+        })
+        const voiceConnection = joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: interaction.guild.id,
+            adapterCreator: interaction.guild.voiceAdapterCreator
 
-client.login(process.env.TOKEN);
+        })
+        const player = createAudioPlayer()
+        voiceConnection.subscribe(player)
+
+        const button = new ButtonBuilder()
+            .setLabel('ㅤ ㅤ ㅤㅤㅤㅤㅤ')
+            .setStyle(ButtonStyle.Link)
+            .setURL(`https://discord.com/channels/${interaction.guild.id}/${voiceChannel.id}`)
+        const actionRow = new ActionRowBuilder().addComponents(button)
+        await interaction.reply({
+            content: '# Enable Audio',
+            components: [actionRow]
+        })
+
+        await new Promise((resolve) => {
+            const handler = (oldState, newState) => {
+                if (
+                    newState.member.id === interaction.user.id &&
+                    newState.channelId === voiceChannel.id
+                ) {
+                    client.off('voiceStateUpdate', handler);
+                    resolve();
+                }
+            };
+
+            client.on('voiceStateUpdate', handler);
+        });
+        await new Promise(resolve => setTimeout(resolve, 200));
+
+
+        player.play(createAudioResource(createReadStream('./src/World/citrus/assets/music/mystery.mp3'), {
+            inputType: StreamType.Arbitrary
+        }))
+        player.on(AudioPlayerStatus.Idle, () => {
+            player.play(createAudioResource(createReadStream('./src/World/citrus/assets/music/mystery.mp3'), {
+                inputType: StreamType.Arbitrary
+            }))
+        })
+    }
+})
+
+client.login(process.env.TOKEN)
